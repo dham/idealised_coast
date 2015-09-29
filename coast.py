ee26723265d96445bd29abaa317b2f553d70f738
David Ham <David.Ham@imperial.ac.uk>
2015-09-29 12:14:21 -0700

    Working quads

--- coast.py
+++ coast.py
@@ -1,8 +1,8 @@
 from firedrake import *
 import numpy as np
 
-degree = 2
-quadrilateral = False
+degree = 1
+quadrilateral = True
 
 # Parameters for Gaussian.
 a = 6
@@ -18,7 +18,14 @@ def function_space(mesh, degree, quadrilateral):
     """Create the required mixed function space."""
 
     if quadrilateral:
-        raise NotImplementedError
+        if degree == 1:
+            u0 = FiniteElement("CG", interval, 1)
+            u1 = FiniteElement("DG", interval, 0)
+            RT1 = HDiv(OuterProductElement(u0, u1)) + HDiv(OuterProductElement(u1, u0))
+            V1 = FunctionSpace(m, RT1)
+            V2 = FunctionSpace(m, "DG", 0)
+        else:
+            raise NotImplementedError
     else:
         if degree == 1:
             V1 = FunctionSpace(m, "RT", 1)
@@ -31,21 +38,41 @@ def function_space(mesh, degree, quadrilateral):
         else:
             raise ValueError("Degree must be 1 or 2")
 
-        return V1 * V2
+    return V1 * V2
 
+
+def mesh(quadrilateral):
+    if not quadrilateral:
+        m = RectangleMesh(50, 10, 100, 20,
+                          quadrilateral=False)
+    else:
+        m_0 = IntervalMesh(50, 100)
+        m = ExtrudedMesh(m_0, 10, 20./10.)
+
+    return m
+
+
+def boundaries(quadrilateral):
+    if not quadrilateral:
+        bcs = [DirichletBC(W[0], (1., 0.), 1),
+               DirichletBC(W[0], (1., 0.), 2),
+               DirichletBC(W[0], (0., 0.), 3),
+               DirichletBC(W[0], (0., 0.), 4)]
+    else:
+        bcs = [DirichletBC(W[0], (1., 0.), 1),
+               DirichletBC(W[0], (1., 0.), 2),
+               DirichletBC(W[0], (0., 0.), "top"),
+               DirichletBC(W[0], (0., 0.), "bottom")]
+    return bcs
 
 # Create the mesh and insert the gaussian bump.
-m = RectangleMesh(50, 10, 100, 20,
-                  quadrilateral=quadrilateral)
+m = mesh(quadrilateral)
 x = m.coordinates.dat.data
 x[:, 1] = 20. + x[:, 1] * (gaussian(x[:, 0]) - 20.)/20.
 
 W = function_space(mesh, 1, quadrilateral)
 
-bcs = [DirichletBC(W[0], (1., 0.), 1),
-       DirichletBC(W[0], (1., 0.), 2),
-       DirichletBC(W[0], (0., 0.), 3),
-       DirichletBC(W[0], (0., 0.), 4)]
+bcs = boundaries(quadrilateral)
 
 solution = Function(W)
 u, p = split(solution)
